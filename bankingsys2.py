63523f1dae3069943c4c053db7d3b94637f27d71
chishtikashifuddin-stack <chishtikashifuddin@gamil.com>
2026-05-15 18:36:29 +0530

    Remove : remove the admin site of check total number of account in bank option

--- bankingsys2.py
+++ bankingsys2.py
@@ -127,22 +127,6 @@ def check():
 
     print("ACCOUNT NOT FOUND")
     check()
-
-
-def total():
-    a = os.popen("type admin_bank_DB.txt").read()
-    a1 = a.split("\n")
-    total_accounts = 0
-    for a2 in a1:
-        if a2 != "":
-            g = json.loads(a2)
-            if "account" in g:
-                total_accounts += 1
-    print("*-------------------------------------------------------------*")
-    print("|           THIS IS TOTAL ACCOUNT IN VERSATILE BANK          |")
-    print("*-------------------------------------------------------------*\n")
-    print(f"TOTAL ACCOUNT : {total_accounts}\n")
-    admin()
     
 # THIS OPTION IS USE FOR USERS
 def user():
